52d606d3ffdc497855e7d3a98bf0d0df8045702a
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-02-23 21:41:47 +0900

    feature. iOS 스와이프 제스쳐, android hardwareBackPress 이벤트 구현 및 기기별 margin-top 적용

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
+import { useEffect, useRef } from "react";
+import { BackHandler, Platform } from "react-native";
 import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
 import * as SplashScreen from "expo-splash-screen";
 
 const App = () => {
+  const webViewRef = useRef<WebView>(null);
+
   const sleep = (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
@@ -12,9 +17,37 @@ const App = () => {
     await SplashScreen.hideAsync();
   };
 
-  delaySplash();
+  const onAndroidBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true;
+    }
+    return false;
+  };
 
-  return <WebView source={{ uri: "https://wagglewaggle.co.kr" }} />;
+  useEffect(() => {
+    delaySplash();
+  }, []);
+
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+      return () =>
+        BackHandler.removeEventListener(
+          "hardwareBackPress",
+          onAndroidBackPress
+        );
+    }
+  }, []);
+
+  return (
+    <WebView
+      ref={webViewRef}
+      allowsBackForwardNavigationGestures
+      source={{ uri: "https://wagglewaggle.co.kr" }}
+      style={{ marginTop: Constants.statusBarHeight }}
+    />
+  );
 };
 
 export default App;
