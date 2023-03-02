583d68018d90129848b79a283bae052b77e22362
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-03-03 00:35:42 +0900

    feature. native-web 유저 위치 통신 구현

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useRef } from "react";
-import { BackHandler, Platform } from "react-native";
+import { BackHandler, Platform, SafeAreaView, Appearance } from "react-native";
 import { WebView } from "react-native-webview";
-import Constants from "expo-constants";
 import * as SplashScreen from "expo-splash-screen";
+import * as Location from "expo-location";
 
 const App = () => {
   const webViewRef = useRef<WebView>(null);
+  const SAFE_AREA_BACKGROUND_COLOR =
+    Appearance.getColorScheme() === "dark" ? "#000" : "#fff";
 
   const sleep = (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -13,7 +15,7 @@ const App = () => {
 
   const delaySplash = async () => {
     await SplashScreen.preventAutoHideAsync();
-    await sleep(2000);
+    await sleep(1000);
     await SplashScreen.hideAsync();
   };
 
@@ -23,6 +25,26 @@ const App = () => {
       return true;
     }
     return false;
+  };
+
+  const getUserLocation = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      webViewRef.current?.postMessage(
+        JSON.stringify({
+          code: "success",
+          latitude: latitude,
+          longitude: longitude,
+        })
+      );
+    } catch (e) {
+      webViewRef.current?.postMessage(
+        JSON.stringify({ code: "fail", errorMsg: "location permission denied" })
+      );
+    }
   };
 
   useEffect(() => {
@@ -41,12 +63,18 @@ const App = () => {
   }, []);
 
   return (
-    <WebView
-      ref={webViewRef}
-      allowsBackForwardNavigationGestures
-      source={{ uri: "https://wagglewaggle.co.kr" }}
-      style={{ marginTop: Constants.statusBarHeight }}
-    />
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: SAFE_AREA_BACKGROUND_COLOR }}
+    >
+      <WebView
+        ref={webViewRef}
+        source={{ uri: "http://192.168.45.139:3000" }}
+        onLoadEnd={getUserLocation}
+        cacheEnabled
+        javaScriptEnabled
+        allowsBackForwardNavigationGestures
+      />
+    </SafeAreaView>
   );
 };
 
